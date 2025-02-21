477afdb6dbea4a65afe4ec1dd8fcbaa27ba31053
JMSJoseph <jsecosky2@gmail.com>
2025-02-21 02:02:05 -0500

    Mostly working svelvet with base level functionality

---
+++ src/lib/AndGate.svelte
@@ -0,0 +1,10 @@
+<script>
+    import image from "./svg/andgate.svg"
+    export let width = 80;
+    export let height = 50;
+</script>
+
+
+
+
+<img src={image} alt="AND Gate" width={width} height={height} />
