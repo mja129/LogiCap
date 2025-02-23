c3dc8e70774616f569f2f4b1d59312bafcbadbf7
Sean Shmulevich <shmul@Ahava.local>
2025-02-23 00:58:17 -0500

    fix node drag-start; create global css styles; Refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,71 @@
 <script lang="ts">
-    import Graph from './lib/Graph.svelte';
+    // import Graph from './lib/Graph.svelte';
+    import { Svelvet, Node, Anchor, Minimap } from 'svelvet';
+    import AndGate from './lib/AndGate.svelte';
+
+    let nodes = [
+        { id: 1, position: { x: 100, y: 100 }},
+        { id: 2, position: { x: 500, y: 100 }}
+    ]
 </script>
 
 <main>
     <h1> Svelvet Graph</h1>
     <div id="graph">
-        <Graph />
+        <Svelvet theme="LogiCap">
+            <Minimap width="{100}" corner="NE" mapColor="blue" slot="minimap" />
+            <Node id="And_Node_1" drop="cursor">
+                <AndGate width={80} height={50} />
+                <div class="input-1">
+                    <Anchor direction="west" input bgColor=red multiple={false} />
+                </div>
+                <div class="input-2">
+                    <Anchor direction="west" input bgColor=red multiple={false}/>
+                </div>
+                <div class="output-1">
+                    <Anchor direction="east" output bgColor=green multiple={false}/>
+                </div>
+            </Node>
+            <Node id="And_Node_2" drop="cursor">
+                <AndGate width={80} height={50} />
+                <div class="input-1">
+                    <Anchor direction="west" input bgColor=red multiple={false} />
+                </div>
+                <div class="input-2">
+                    <Anchor direction="west" input bgColor=red multiple={false}/>
+                </div>
+                <div class="output-1">
+                    <Anchor direction="east" output bgColor=green multiple={false}/>
+                </div>
+            </Node>
+        </Svelvet>
     </div>
+
 </main>
+
+<style>
+    :global(.svelvet-node) {
+        background: none !important;
+        box-shadow: none !important;
+        border: none !important;
+        padding: 0 !important;
+        width: auto !important;
+        height: auto !important;
+    }
+    .input-1 {
+        position: absolute;
+        top: 14.5%;
+        left: 7.5%;
+    }
+    .input-2 {
+        position: absolute;
+        top: 51.5%;
+        left: 7.5%;
+    }
+    .output-1 {
+        position: absolute;
+        top: 35%;
+        right: 10%
+    }
+
+</style>
