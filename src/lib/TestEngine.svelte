a47dde2869a7ac713e28cf43e1d98ccdac51c784
JMSJoseph <jsecosky2@gmail.com>
2025-03-13 22:11:42 -0400

    Fixed my name in index.html lol, Also added much better looking wire monitoring. Still issues with Inputs so its using a test circuit

--- src/lib/TestEngine.svelte
+++ src/lib/TestEngine.svelte
@@ -1,94 +1,155 @@
 <script lang="ts">
     import { HeadlessCircuit, engines as Engines } from 'custom_digitaljs'
-    import { circuitStore } from './circuitStore'
+    import { circuitStore } from '../lib/circuitStore';
     import sampleCircuit from '../assets/CircuitJsonSampleData/testcircuit.json'
 
+    Object.defineProperty(HeadlessCircuit.prototype, "running", {
+    get() {
+        //console.warn("Overriding `running` getter at runtime.");
+        return false;
+    }
+});
+    
     //Sort of hacky way to just remake a circuit each time it updates
     //For now doesnt work when attempting to simulate anything because json is messed up
+
+    let circuitData;
+    let circuit: HeadlessCircuit;
+    
+    $: circuitData = $circuitStore; 
+    
+
     /*
-let circuitData;
-let circuit;
+    $: if (circuitData) {
+        console.log(circuitData)
+        circuit = new HeadlessCircuit(circuitData);
+        console.log(circuit.getLabelIndex());
+    }
+    */
 
-$: circuitData = $circuitStore; 
-
-$: if (circuitData) {
-    circuit = new HeadlessCircuit(circuitData);
-    console.log("Created Headless Circut:", circuit);
-}
-*/
-
-    let currentCircuit = new HeadlessCircuit(sampleCircuit) //Test clock->not->output
-    let running = false
-    let intervalId: any = null
-    let previousJSON = ''
-
+    
+    let currentCircuit = new HeadlessCircuit(sampleCircuit); //Test clock->not->output
+    let running = false;
+    let intervalId: any = null;
+    let previousJSON = "";
+    
     //For key listener
-    function updateRunning() {
-        running = !running
-        console.log(`Simulation running: ${running}`)
-        if (running) {
-            runFunction()
-        } else {
-            clearInterval(intervalId)
+    function updateRunning()
+    {
+        running = !running;
+        console.log(`Simulation running: ${running}`);
+        if(running)
+        {
+           startSim();
         }
     }
-
+    
     //Logs the bit values of the input and output signals
-    function logValues() {
+    /*
+    function logValues()
+    {
         let cells = currentCircuit.getLabelIndex()
-        let devices = cells.devices
-        let bitValues: Record<string, any> = {}
-        Object.entries(devices).forEach(
-            (
-                device // Loop through the array
-            ) => {
-                let dev = device[1] as any
-                //console.log(device);
-                if (dev.attributes?.inputSignals?.in) {
-                    // Ensure "in" exists
-                    let _avec = dev.attributes.inputSignals.in._avec?.['0'] // Current value
-                    let _bvec = dev.attributes.inputSignals.in._bvec?.['0'] // Buffered value?
-                    bitValues[dev.attributes.label + ' Input Sig'] = {
-                        _avec,
-                        _bvec,
-                    }
-                }
-                if (dev.attributes?.outputSignals?.out) {
-                    // Ensure "out" exists
-                    let _avec = dev.attributes.outputSignals.out._avec?.['0'] // Current value
-                    let _bvec = dev.attributes.outputSignals.out._bvec?.['0'] // Buffered value?
-                    bitValues[dev.attributes.label + ' Output Sig'] = {
-                        _avec,
-                        _bvec,
-                    }
-                }
+        let devices = cells.devices;
+        let bitValues: Record<string, any> = {};
+        Object.entries(devices).forEach(device => // Loop through the array
+        {  
+            let dev = device[1] as any;
+            //console.log(device);
+            if (dev.attributes?.inputSignals?.in) // Ensure "in" exists
+            {  
+                let _avec = dev.attributes.inputSignals.in._avec?.["0"];  // Current value
+                let _bvec = dev.attributes.inputSignals.in._bvec?.["0"];  // Buffered value?
+                bitValues[dev.attributes.label + " Input Sig"] = { _avec, _bvec };
             }
-        )
-        return JSON.stringify(bitValues, null, 2)
+            if (dev.attributes?.outputSignals?.out) // Ensure "out" exists
+            {  
+                let _avec = dev.attributes.outputSignals.out._avec?.["0"];  // Current value
+                let _bvec = dev.attributes.outputSignals.out._bvec?.["0"];  // Buffered value?
+                bitValues[dev.attributes.label + " Output Sig"] = { _avec, _bvec };
+            }
+        });
+        return JSON.stringify(bitValues, null, 2);
+    
     }
-
+    */
+    
+    function startSim()
+    {
+        if (running) {
+            currentCircuit.updateGates();
+            setTimeout(startSim, 10);
+        }
+    }
+    
+    function wireMonitoring(wire: any, tick:number)
+    {
+        let logicValue: number;
+        //console.log(wire)
+        if(wire[1].attributes.signal)
+        {
+            logicValue = wire[1].attributes.signal._avec[0] & wire[1].attributes.signal._bvec[0];
+            console.log(`${wire[0]} has changed signal to ${logicValue} at tick ${tick}`);
+        }
+    }
+    
     //Possibly better way to do this, as this basically is what .start() does
     //But i want to be able to log values as it runs
-    function runFunction() {
-        ;(intervalId = setInterval(() => {
-            if (!running) {
-                return
-            }
-            currentCircuit.updateGates() //This moves the simulation by 1 tick
-            let currentJSON = logValues()
-            if (currentJSON != previousJSON) {
-                //Only log changes
-                console.log(`Tick: ${currentCircuit.tick}`)
-                console.log(currentJSON)
-            }
-            previousJSON = currentJSON
-        })),
-            10
-    }
     //Just a keylistener for R
-    window.addEventListener('keydown', (event) => {
-        if (event.key === 'r') {
-            updateRunning()
+    window.addEventListener("keydown", (event) => {
+        if (event.key === "r") {
+            updateRunning();
         }
-    })
-</script>
+        if (event.key === "ArrowRight")
+        {
+            currentCircuit.updateGates();
+        }
+        if (event.key === "ArrowUp")
+        {
+            currentCircuit.updateGatesNext();
+        }
+        //Temp testing stuff disabled
+        /*
+        if (event.key === "ArrowLeft")
+        {
+            circuit = new HeadlessCircuit(circuitData);
+            console.log(circuit.getLabelIndex());
+            let wires = circuit.getLabelIndex().wires
+    
+            Object.entries(wires).forEach(wire => {
+                circuit.monitorWire(circuit.findWireByLabel(wire[0]), (tick: number) => wireMonitoring(wire[0], tick));
+            });
+        }
+        if (event.key == "ArrowDown")
+        {
+            inputSetter();
+        }
+        */
+    });
+
+    //Need to rewrite this to use setInput, changing avec does nothing as it doesnt update
+    /*
+    function inputSetter()
+    {
+        let userInput: string | null;
+        userInput = prompt("Enter a value:");
+        if(userInput == null)
+        {
+            userInput = "";
+        }
+        console.log(circuit.getLabelIndex().devices[userInput].attributes.outputSignals.out._avec[0]);
+        console.log(circuit.getLabelIndex().devices[userInput].attributes.outputSignals.out._bvec[0]);
+        circuit.getLabelIndex().devices[userInput].attributes.outputSignals.out._avec[0] = 1;
+        circuit.getLabelIndex().devices[userInput].attributes.outputSignals.out._bvec[0] = 1;
+        console.log(circuit.getLabelIndex().devices[userInput].attributes.outputSignals.out._avec[0]);
+        console.log(circuit.getLabelIndex().devices[userInput].attributes.outputSignals.out._bvec[0]);
+        circuit.synchronize();
+    }
+    */
+    
+    let wires = currentCircuit.getLabelIndex().wires
+    
+    Object.entries(wires).forEach(wire => {
+        currentCircuit.monitorWire(currentCircuit.findWireByLabel(wire[0]), (tick: number) => wireMonitoring(wire, tick));
+    });
+    
+    </script>
